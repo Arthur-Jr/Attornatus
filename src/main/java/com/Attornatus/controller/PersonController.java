915a90b49467684a739b8aebfe44427c55d7199f
Arthur Jr <arthurjr1906@gmail.com>
2023-01-11 20:53:52 -0300

    feat: add editPerson method on controller

--- src/main/java/com/Attornatus/controller/PersonController.java
+++ src/main/java/com/Attornatus/controller/PersonController.java
@@ -7,12 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Attornatus.dto.PersonDto;
+import com.Attornatus.dto.PersonEditDto;
 import com.Attornatus.model.Person;
 import com.Attornatus.service.PersonService;
 
@@ -34,5 +37,12 @@ public class PersonController {
   @GetMapping
   public List<Person> listPerson() {
     return this.service.listPerson();
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Person> editPerson(@Valid @RequestBody PersonEditDto person, @PathVariable Long id) {
+    return ResponseEntity.status(HttpStatus.OK)
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(this.service.editPerson(person, id));
   }
 }
