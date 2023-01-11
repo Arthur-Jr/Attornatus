ab2c326cbe7e01afd9b4b29fba4b29a5bcd0d0da
Arthur Jr <arthurjr1906@gmail.com>
2023-01-11 17:30:56 -0300

    feat: Create Person Entity

---
+++ src/main/java/com/Attornatus/model/Person.java
@@ -0,0 +1,45 @@
+package com.Attornatus.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  @Column(name = "birthdate")
+  private LocalDate birthDate;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public LocalDate getBirthDate() {
+    return this.birthDate;
+  }
+
+  public void setBirthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+  }
+}
