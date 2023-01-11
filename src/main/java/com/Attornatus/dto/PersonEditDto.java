787a6964cbd8b78e2c2c09275a21e9c80c237184
Arthur Jr <arthurjr1906@gmail.com>
2023-01-11 20:52:02 -0300

    feat: create Person edit DTO

---
+++ src/main/java/com/Attornatus/dto/PersonEditDto.java
@@ -0,0 +1,59 @@
+package com.Attornatus.dto;
+
+import com.Attornatus.exception.ErrorMessages;
+
+import jakarta.validation.constraints.Size;
+
+public class PersonEditDto {
+  @Size(min = 3, message = ErrorMessages.nameSize)
+  private String name;
+
+  private String birthDate;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBirthDate() {
+    return this.birthDate;
+  }
+
+  public void setBirthDate(String birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    PersonEditDto other = (PersonEditDto) obj;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (birthDate == null) {
+      if (other.birthDate != null)
+        return false;
+    } else if (!birthDate.equals(other.birthDate))
+      return false;
+    return true;
+  }
+}
