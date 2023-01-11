c1ec906f72bb058fd793c18796554d2c39ccaf72
Arthur Jr <arthurjr1906@gmail.com>
2023-01-11 20:52:37 -0300

    feat: add edit Person tests on service Tests

--- src/test/java/com/Attornatus/service/PersonServiceTests.java
+++ src/test/java/com/Attornatus/service/PersonServiceTests.java
@@ -8,6 +8,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 
@@ -19,6 +21,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.Attornatus.dto.PersonDto;
+import com.Attornatus.dto.PersonEditDto;
 import com.Attornatus.model.Person;
 import com.Attornatus.repository.PersonRepository;
 import com.Attornatus.util.PersonDataExample;
@@ -75,5 +78,68 @@ public class PersonServiceTests {
 
     assertEquals(0, personListSize);
     verify(repo, times(1)).findAll();
+  }
+
+  @Test
+  @DisplayName("Edit Person test: name edit test")
+  void name_edit_test() {
+    PersonEditDto payload = new PersonEditDto();
+    payload.setName("Test_2");
+
+    Person person = new Person();
+    person.setName(PersonDataExample.getName());
+    person.setBirthDate(PersonDataExample.getBirthDateFormatted());
+    person.setId(Long.valueOf(1));
+    when(this.repo.getReferenceById(Long.valueOf(1))).thenReturn(person);
+
+    person.setName(payload.getName());
+    when(this.repo.save(person)).thenReturn(person);
+
+    Person createdPerson = this.service.editPerson(payload, Long.valueOf(1));
+    assertEquals("Test_2", createdPerson.getName());
+    assertEquals(PersonDataExample.getBirthDateFormatted(), createdPerson.getBirthDate());
+    assertEquals(1, createdPerson.getId());
+    verify(repo, times(1)).save(person);
+    verify(repo, times(1)).getReferenceById(Long.valueOf(1));
+  }
+
+  @Test
+  @DisplayName("Edit Person test: birthdate edit test")
+  void birthdate_edit_test() {
+    PersonEditDto payload = new PersonEditDto();
+    payload.setBirthDate("12/12/2010");
+
+    Person person = new Person();
+    person.setName(PersonDataExample.getName());
+    person.setBirthDate(PersonDataExample.getBirthDateFormatted());
+    person.setId(Long.valueOf(1));
+    when(this.repo.getReferenceById(Long.valueOf(1))).thenReturn(person);
+
+    LocalDate dateToEdit = LocalDate.parse(payload.getBirthDate(), DateTimeFormatter.ofPattern("dd/MM/yyy"));
+    person.setBirthDate(dateToEdit);
+    when(this.repo.save(person)).thenReturn(person);
+
+    Person createdPerson = this.service.editPerson(payload, Long.valueOf(1));
+    assertEquals(PersonDataExample.getName(), createdPerson.getName());
+    assertEquals(dateToEdit, createdPerson.getBirthDate());
+    assertEquals(1, createdPerson.getId());
+    verify(repo, times(1)).save(person);
+    verify(repo, times(1)).getReferenceById(Long.valueOf(1));
+  }
+
+  @Test
+  @DisplayName("Edit Person test: should throw if date format is invalid")
+  void should_thow_if_birthdate_format_is_invalid() {
+    PersonEditDto payload = new PersonEditDto();
+    payload.setBirthDate("2010/12/12");
+
+    Throwable exception = assertThrows(DateTimeParseException.class, () -> {
+      this.service.editPerson(payload, Long.valueOf(1));
+    });
+
+    assertNotNull(exception);
+    assertEquals("Text '2010/12/12' could not be parsed at index 2", exception.getMessage());
+    verify(repo, times(0)).save(any(Person.class));
+    verify(repo, times(1)).getReferenceById(Long.valueOf(1));
   }
 }
