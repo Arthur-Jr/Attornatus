5d5c148e2fb7062b70e6cdb4432a62cf4c858efa
Arthur Jr <arthurjr1906@gmail.com>
2023-01-12 02:25:27 -0300

    feat: repositories update

--- src/main/java/com/Attornatus/repository/CityRepository.java
+++ src/main/java/com/Attornatus/repository/CityRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.Attornatus.model.City;
 
 public interface CityRepository extends JpaRepository<City, Long> {
-  
+
+  City findByCityName(String cityName);
 }
--- src/main/java/com/Attornatus/repository/StreetRepository.java
+++ src/main/java/com/Attornatus/repository/StreetRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.Attornatus.model.Street;
 
 public interface StreetRepository extends JpaRepository<Street, Long> {
-  
+  Street findByCep(String cep);
+
+  Street findByStreetName(String streetName);
 }
