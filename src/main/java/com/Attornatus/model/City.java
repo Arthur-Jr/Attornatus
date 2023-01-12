d48316fed60c42eeb4b4f50e8964e51f8ecf3cc9
Arthur Jr <arthurjr1906@gmail.com>
2023-01-12 01:16:07 -0300

    feat: create city entity

---
+++ src/main/java/com/Attornatus/model/City.java
@@ -0,0 +1,33 @@
+package com.Attornatus.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class City {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "city_name")
+  private String cityName;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getCityName() {
+    return cityName;
+  }
+
+  public void setCityName(String cityName) {
+    this.cityName = cityName;
+  }
+}
